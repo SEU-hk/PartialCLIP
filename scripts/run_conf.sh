c9246534904a1cd7724d8ea3c1212b4e87742b65
SEU-hk <hekuang2000427@163.com>
2025-11-02 07:24:56 +0000

    11.2

---
+++ scripts/run_conf.sh
@@ -0,0 +1,31 @@
+python main.py -d cifar10 -m zsclip_vit_b16 infer_train True
+
+python main.py -d cifar100 -m zsclip_vit_b16 infer_train True
+
+python main.py -d cars196 -m zsclip_vit_b16 infer_train True
+
+python main.py -d dogs120 -m zsclip_vit_b16 infer_train True
+
+python main.py -d cub200 -m zsclip_vit_b16 infer_train True
+
+python main.py -d fgvc100 -m zsclip_vit_b16 infer_train True
+
+python main.py -d cifar10_ir100 -m zsclip_vit_b16 infer_train True
+
+python main.py -d cifar10_ir150 -m zsclip_vit_b16 infer_train True
+
+python main.py -d cifar10_ir200 -m zsclip_vit_b16 infer_train True
+
+python main.py -d cifar10_ir250 -m zsclip_vit_b16 infer_train True
+
+python main.py -d cifar100_ir20 -m zsclip_vit_b16 infer_train True
+
+python main.py -d cifar100_ir50 -m zsclip_vit_b16 infer_train True
+
+python main.py -d cifar100_ir100 -m zsclip_vit_b16 infer_train True
+
+python main.py -d cifar100_ir150 -m zsclip_vit_b16 infer_train True
+
+python main.py -d places_lt -m zsclip_vit_b16 infer_train True
+
+python main.py -d imagenet_lt -m zsclip_vit_b16 infer_train True
